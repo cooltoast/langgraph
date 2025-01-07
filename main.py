528d78d5836183a33d233a9f2618c27a6199e1a1
cooltoast <mpatel678@gmail.com>
2025-01-06 19:41:42 -0800

    step 3: add memory

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from typing_extensions import TypedDict
 
 from env import set_keys
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
 
 set_keys()
+
+DEFAULT_THREAD_ID = "default"
 
 
 class State(TypedDict):
@@ -19,6 +22,7 @@ class State(TypedDict):
 tavily_search = TavilySearchResults(max_results=2)
 tools = [tavily_search]
 llm = ChatAnthropic(model="claude-3-5-sonnet-20240620").bind_tools(tools)
+memory = MemorySaver()
 
 graph_builder = StateGraph(State)
 
@@ -33,23 +37,27 @@ graph_builder.set_entry_point("chatbot")
 graph_builder.add_conditional_edges("chatbot", tools_condition)
 graph_builder.add_edge("tools", "chatbot")
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=memory)
 
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [("user", user_input)]}):
-        for value in event.values():
-            print("Assistant: ", value["messages"][-1].content)
+def stream_graph_updates(user_input: str, thread: str):
+    for value in graph.stream(
+        {"messages": [("user", user_input)]},
+        {"configurable": {"thread_id": thread}},
+        stream_mode="values",
+    ):
+        value["messages"][-1].pretty_print()
 
 
 def run_chatbot():
     while True:
         user_input = input("User: ")
+        thread = input("Thread: ") or DEFAULT_THREAD_ID
         if user_input.lower() in ["quit", "exit", "q"]:
             print("Goodbye!")
             break
 
-        stream_graph_updates(user_input)
+        stream_graph_updates(user_input, thread)
 
 
 if __name__ == "__main__":
